ad37ced34805adc9fd0dddb563275cf6b58486b5
Man Ho <hmman.ct@gmail.com>
2025-02-13 11:59:55 +0700

    Create BaseViewModel

---
+++ presentation/src/main/java/com/homanad/android/project/base/common/BaseViewModel.kt
@@ -0,0 +1,51 @@
+package com.homanad.android.project.base.common
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import java.util.concurrent.atomic.AtomicBoolean
+
+abstract class BaseViewModel<Intent, State>(
+    configs: ViewModelConfigs
+) : ViewModel() {
+
+    private val initialised = AtomicBoolean(false)
+
+    private val _intentChannel = Channel<Intent>(configs.intentChannelSize)
+
+    private val _stateChannel = Channel<State>(configs.stateChannelSize)
+
+    init {
+        start()
+    }
+
+    private fun start() {
+        if (initialised.compareAndSet(false, true)) {
+            onFirstStart()
+            viewModelScope.launch(Dispatchers.IO) {
+                for (intent in _intentChannel) {
+                    launch {
+                        processIntent(intent)
+                    }
+                }
+            }
+        }
+    }
+
+    protected open fun onFirstStart() {}
+
+    protected abstract fun processIntent(intent: Intent)
+
+    protected fun emitState(state: State) {
+        viewModelScope.launch {
+            _stateChannel.send(state)
+        }
+    }
+}
+
+data class ViewModelConfigs(
+    val intentChannelSize: Int = Channel.UNLIMITED,
+    val stateChannelSize: Int = Channel.BUFFERED,
+)
